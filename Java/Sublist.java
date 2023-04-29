9212434ec94959b3762b796e1179c65a5858cd72
VirajChoramale <virajchormale71@gmail.com>
2023-04-30 01:03:51 +0530

    x
    d

---
+++ Java/Sublist.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Sublist{
+    public static void main(String[] args) {
+    
+    ArrayList<Integer>arr=new ArrayList<Integer>();
+    for(int i=1;i<6;i++){
+        arr.add(i);
+    }
+   //[1,2,3,4];
+   for(int i=0;i<arr.size()/2;i++){
+        ArrayList<Integer> sub=new ArrayList<Integer>();
+        sub.add(i);
+
+   }
+    
+    }
+}
